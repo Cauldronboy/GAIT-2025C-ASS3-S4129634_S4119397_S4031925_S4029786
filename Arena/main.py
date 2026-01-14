16da08c1ab98de4bdfb1bee6f3c9777275eb7aa7
Duc Nguyen <s4129634@rmit.edu.vn>
2026-01-14 12:31:40 +0700

    feat(main.py): Import package

---
+++ Arena/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+GAIT Assignment 3 - Part 2: Deep RL real time Arena training
+"""
+
+import sys
+import pygame
+from typing import Optional, Tuple
+from environment import Arena, ArenaRenderer
